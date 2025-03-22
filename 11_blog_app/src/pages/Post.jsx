7fdd5e736dd42086f96a3dec708c5b94404e38ee
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-03-22 10:42:32 +0530

    Post page

--- 11_blog_app/src/pages/Post.jsx
+++ 11_blog_app/src/pages/Post.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react"
 import React from 'react'
+import { Link, useNavigate, useParams } from "react-router-dom"
+import appwriteService from '../appwrite/config'
+import Button from '../components/Button'
+import Container from '../components/container/Container'
+import parse from 'html-react-parser'
 
 function Post() {
   return (
@@ -7,5 +13,6 @@ function Post() {
     </div>
   )
 }
+//4.19
 
 export default Post
