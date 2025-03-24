3d2ea789a37d08ab039ceaac642b1cdbf1f5b155
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-03-24 09:40:51 +0530

    All done

--- 11_blog_app/src/pages/AllPosts.jsx
+++ 11_blog_app/src/pages/AllPosts.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import appwritreService from "../appwrite/config"
+import { useState } from "react"
+import Container from "../components/container/Container"
+import PostCard from "../components/PostCard"
 
 function AllPosts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    appwritreService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+  }, [])
+
+
+
   return (
-    <div>
-      All Post
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {
+            posts.map((post) => (
+              <div className="p-2 w-1/4" key={post.$id}>
+                <PostCard {...post} />
+              </div>
+            ))
+          }
+        </div>
+      </Container>
     </div>
   )
 }
--- 11_blog_app/src/pages/Home.jsx
+++ 11_blog_app/src/pages/Home.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import appwritreService from "../appwrite/config"
+import { useState } from "react"
+import Container from "../components/container/Container"
+import PostCard from "../components/PostCard"
 
 function Home() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    appwritreService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+  }, [])
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            <h1>Login to read posts</h1>
+          </div>
+        </Container>
+      </div>
+    )
+  }
+  //TODO: add case for array lengh is 0 -task
   return (
-    <div className='text-7xl'>
-      Home
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {
+            posts.map((post) => (
+              <div className="p-2 w-1/4" key={post.$id}>
+                <PostCard {...post} />
+              </div>
+            ))
+          }
+        </div>
+      </Container>
     </div>
   )
 }
